c8af83aa5c119daf7c7f4d3b1cb9f87aa4e70f6c
Vegard Stenvik <vegard.stenvik@bouvet.no>
2022-12-01 08:15:12 +0100

    init

---
+++ Day1/Day1.fsx
@@ -0,0 +1,33 @@
+﻿
+open System
+open System.Collections.Generic
+open System.IO
+
+let example = """1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"""
+
+let toInt (x:string) = match Int32.TryParse x with
+                        | true, i -> Some i
+                        | _ -> None
+let input = File.ReadAllLines "./Day1/input.txt"
+
+
+let list = List<List<string>>()
+list.Add(List())
+
+input
+|> Seq.iter (fun inp ->
+             if inp = "" then list.Add(List())
+             else )
