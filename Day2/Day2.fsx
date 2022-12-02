a52d5a5703a0884d40f721ee954b2fcae501d444
Vegard Stenvik <vegard.stenvik@bouvet.no>
2022-12-02 06:34:31 +0100

    Day2 part 2

--- Day2/Day2.fsx
+++ Day2/Day2.fsx
@@ -1,28 +1,47 @@
 ﻿open System
 open System.IO
-
-
 type Move = Rock | Paper | Scissors
 type Game = (Move * Move)
 
 let parseMove = function
-          | "A" | "X" -> Rock
-          | "B" | "Y" -> Paper
-          | "C" | "Z" -> Scissors
-          | s -> raise (Exception($"{s} is not a valid input"))
-          
+    | "A" -> Rock
+    | "B" -> Paper
+    | "C" -> Scissors
+    | s -> raise (Exception($"{s} is not a valid input"))
+
+let parseCounterMove = function
+    | Rock, "X" -> Scissors
+    | Rock, "Y" -> Rock
+    | Rock, "Z" -> Paper
+    | Paper, "X" -> Rock
+    | Paper, "Y" -> Paper
+    | Paper, "Z" -> Scissors
+    | Scissors, "X" -> Paper
+    | Scissors, "Y" -> Scissors
+    | Scissors, "Z" -> Rock
+    | (_, s) -> raise (Exception($"{s} is not a valid countermove"))
+ 
 let parseRow (str: string) =
     let split = str.Split(" ", StringSplitOptions.RemoveEmptyEntries)
-    (parseMove split[0], parseMove split[1])
-let getWinScore (game: Game) =
-    match game with
-    | Rock, Rock | Paper, Paper | Scissors, Scissors -> 3
-    | Rock, Paper | Paper, Scissors | Scissors, Rock -> 6
+    let move = parseMove split[0]
+    (move, parseCounterMove (move, split[1]))
+
+let getWinScore = function
+    // Ties
+    | Rock, Rock
+    | Paper, Paper
+    | Scissors, Scissors -> 3
+    // Wins
+    | Rock, Paper
+    | Paper, Scissors
+    | Scissors, Rock -> 6
+    // Losses
     | _ -> 0
+
 let getMoveScore = function
-                   | Rock -> 1
-                   | Paper -> 2
-                   | Scissors -> 3
+    | Rock -> 1
+    | Paper -> 2
+    | Scissors -> 3
 
 let getGameScore (game: Game) =
     let win = getWinScore game
