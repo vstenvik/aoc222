b40ed632b4165b661022a12d2b70d5309a325c43
Vegard Stenvik <vegard.stenvik@bouvet.no>
2022-12-02 06:27:36 +0100

    Day2 part 1

--- Day1/Day1.fsx
+++ Day1/Day1.fsx
@@ -1,24 +1,8 @@
 ﻿open System
 open System.IO
-
-let example = """1000
-2000
-3000
-
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000"""
-
 let toInt (x:string) = Int32.Parse x
-let input = File.ReadAllText "./Day1/input.txt"
 
+let input = File.ReadAllText "./Day1/input.txt"
 
 input.Split("\r\n\r\n")
 |> Array.map (fun str ->
---
+++ Day2/Day2.fsx
@@ -0,0 +1,36 @@
+﻿open System
+open System.IO
+
+
+type Move = Rock | Paper | Scissors
+type Game = (Move * Move)
+
+let parseMove = function
+          | "A" | "X" -> Rock
+          | "B" | "Y" -> Paper
+          | "C" | "Z" -> Scissors
+          | s -> raise (Exception($"{s} is not a valid input"))
+          
+let parseRow (str: string) =
+    let split = str.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    (parseMove split[0], parseMove split[1])
+let getWinScore (game: Game) =
+    match game with
+    | Rock, Rock | Paper, Paper | Scissors, Scissors -> 3
+    | Rock, Paper | Paper, Scissors | Scissors, Rock -> 6
+    | _ -> 0
+let getMoveScore = function
+                   | Rock -> 1
+                   | Paper -> 2
+                   | Scissors -> 3
+
+let getGameScore (game: Game) =
+    let win = getWinScore game
+    let move = game |> snd |> getMoveScore
+    win + move
+
+let input = File.ReadAllLines "./Day2/input.txt"
+
+input
+|> Seq.map (parseRow >> getGameScore)
+|> Seq.sum
