4ac8635d202f3d99a6090f5ae2ec8bc552c2909d
Vegard Stenvik <vegard.stenvik@bouvet.no>
2022-12-12 20:34:48 +0100

    Day9

--- Day9/Day9.fsx
+++ Day9/Day9.fsx
@@ -8,10 +8,9 @@ type Move =
     | Right of count: int
     | Down of count: int
 
-
 module Move =
     let regex = Regex("(?<dir>\S) (?<count>\d+)")
-    let parse (str: string) =
+    let parse str =
         let m = regex.Match(str)
         if m.Success then
             let count = int <| m.Groups["count"].Value
@@ -29,8 +28,10 @@ module Move =
         | Right c -> List.replicate c (Right 1)
         | Down c -> List.replicate c (Down 1)
 
-type Position = int * int
 
+
+
+type Position = int * int
 module Position =
     let apply (x, y) move =
         match move with
@@ -38,35 +39,50 @@ module Position =
         | Down c -> (x, y - c)
         | Left c -> (x - c, y)
         | Right c -> (x + c, y)
+        
+    let follow (x1, y1) (x2, y2) =
+        let dx = Math.Clamp(x1-x2 ,-1, 1)
+        let dy = Math.Clamp(y1-y2 ,-1, 1)
+        x2 + dx, y2 + dy
+        
     let isTooFar (x1: int, y1: int) (x2,y2) =
         (Math.Abs(x2 - x1) >= 2) || (Math.Abs(y2-y1) >= 2)
 
-type MoveState = {
+
+
+
+type RopeState = {
     visited: Set<Position>
     head: Position
-    tail: Position
+    tail: Position list
 }
-
-module MoveState =
+module RopeState =
+    let rec updateTail head tail =
+        match tail with
+        | [] -> []
+        | current::rest ->
+            let updatedPos = if Position.isTooFar head current
+                             then  Position.follow head current
+                             else current
+            updatedPos::(updateTail updatedPos (rest))
+            
     let apply state move =
         let newHead = Position.apply state.head move
-        let newTail = if Position.isTooFar state.tail newHead
-                      then state.head
-                      else state.tail
+        let newTail = updateTail newHead state.tail
         { state with
             head = newHead
             tail = newTail
-            visited = Set.add newTail state.visited }
+            visited = Set.add (List.last newTail) state.visited }
         
-let initialState = {
-    head = 0,0
-    tail = 0,0
-    visited = set [(0,0)]
-}
+    let initialState = {
+        head = 0,0
+        tail = List.replicate 9 (0,0)
+        visited = set [(0,0)]
+    }
 
 let example = File.ReadAllLines "./Day9/input.txt"
               |> Seq.map Move.parse
               |> Seq.collect Move.expand
-              |> Seq.fold MoveState.apply initialState
+              |> Seq.fold RopeState.apply RopeState.initialState
 
 example.visited.Count
